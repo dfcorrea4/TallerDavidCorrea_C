f33ae79dc16dca540be04afb3af5f2b062651733
dfcorrea4 <136023452+dfcorrea4@users.noreply.github.com>
2023-06-16 01:35:16 +0000

    Importar librerias

---
+++ geometria.py
@@ -0,0 +1,20 @@
+from math import sqrt 
+def distancia(x1, y1, x2, y2):
+    """
+    Calcula la distancia euclidiana entre dos puntos en un plano cartesiano.
+
+    Parámetros:
+
+    x1 (float): La coordenada x del primer punto.
+
+    y1 (float): La coordenada y del primer punto.
+
+    x2 (float): La coordenada x del segundo punto.
+
+    y2 (float): La coordenada y del segundo punto.
+
+    Retorna:
+    float: La distancia euclidiana entre los dos puntos.
+    """
+    resultado = sqrt((x2-x1)**2 + (y2-y1)**2)
+    return resultado 
---
+++ programa.py
@@ -0,0 +1,7 @@
+from Matematicas import sumar, restar, dividir, multiplicar
+from geometria import distancia
+print("Suma: ", sumar(2,3))
+print("Resta: ", restar(4, 2))
+print("División: ", dividir(2, 6))
+print("Multiplicación: ", multiplicar(3, 5))
+print("Distancia: ", distancia(1, 2, 4, 5))
